8ffa0260c830c75f80d4953ba356ef6b32d7d1f9
rakejyals <jakepryals@protonmail.com>
2026-04-09 13:36:06 -0400

    Hopefully wrapping up d3 coding

--- D3/pitstop-vis.js
+++ D3/pitstop-vis.js
@@ -1,4 +1,4 @@
-// TODO support for gap/delta overflow and underflow
+// TODO support for gap/delta overflow and underflow (outside scope of course project)
 const load_data = function (i) {
 
     d3.csv(`${path_prefix}${i}${path_suffix}`, function (d) {
@@ -12,7 +12,7 @@ const load_data = function (i) {
             // Math.random() is uniform [0,1]
         }
         return {
-            //pos : +d["position"],
+            pos : +d["position"],
             class : d["class"],
             rel_laps : +d["rel_laps"],
             lap_delta : +d["lap_delta"],
@@ -36,14 +36,13 @@ const load_data = function (i) {
 
         const gap_extent = Math.max(...d3.extent(data, d => d.gap).map(Math.abs)) + d3.max(data, d => d.gap)
         const gap_scale = d3.scaleLinear()  // lap_delta also uses this scale
-            .domain([-80, 80])  // Temporary!
+            .domain([-75, 75])  // Temporary!
             .range([0, width])
 
         const class_scale = d3.scaleOrdinal()
             .domain(["HY", "GT3"])
             .range(["circle", "rect"])
 
-        // TODO rel_laps, laps_remaining (reconcile 2 color axes! May need to group, or use opacity?)
         const hue_scale = d3.scaleDiverging()
             .domain([-1, 0, 1])  // Hopefully this will clip?
             .range(["blue", "green", "red"])
@@ -54,14 +53,32 @@ const load_data = function (i) {
 
         const size_scale = d3.scaleOrdinal()  // For emphasizing important marks
             .domain([true, false])
-            .range([1.5 * mark_radius, mark_radius])
+            .range([2.5 * mark_radius, mark_radius])
 
         // Axis
         svg.select("#axis")
             .attr("transform", `translate(0, ${jitter_height * 1.1 + 10})`)
             .call(d3.axisBottom(gap_scale))
+        
+        // Axis label
+        svg.select("#axis-label")
+            .select("text")
+            .text("Gap to Focused Car (sec)")
+            .attr("x", gap_scale(0) - 70)
+            .attr("y", jitter_height * 1.1 + 50)
 
 
+        // Pitstop Rect
+        svg.select("#pitstop-rect")
+            .select("rect")
+                .attr("x", gap_scale(-pitstop_length))
+                .attr("y", 0)
+                .attr("width", gap_scale(0) - gap_scale(-pitstop_length))
+                .attr("height", jitter_height + 10)
+                .style("fill", "yellow")
+                .style("fill-opacity", 0.25)
+
+            
         // Tails
         svg.select("#tails")
             .selectAll("line")
@@ -73,8 +90,7 @@ const load_data = function (i) {
                 .attr("x1", d => gap_scale(d.gap))
                 .attr("y1", d => d.jitter)
                 .attr("x2", d => gap_scale(d.gap + d.lap_delta))
-                .attr("y2", d => d.jitter)  // Keep in mind that circ pos is measured by center, rect by top-left corner
-                // (apply translation, subtract half of width, add half height)
+                .attr("y2", d => d.jitter)
 
         // Marks
         const class_groups = d3.group(data, d => d.class)
@@ -97,17 +113,25 @@ const load_data = function (i) {
                     .attr("y", d => d.jitter - size_scale(d.is_self))
                     .attr("width", d => 2 * size_scale(d.is_self))  // Needs to be larger!
                     .attr("height", d => 2 * size_scale(d.is_self))
+                    // Selection interaction (TODO figure out why click doesn't work when box overlaps)
+                    .on("click", function (event, d) {
+                        const self = d3.select(this)
+                        //Resize
+                        self.attr("r", size_scale(true))
+                        self.attr("width", 2 * size_scale(true))
+                        self.attr("height", 2 * size_scale(true))
+                        .attr("x", d => gap_scale(d.gap) - size_scale(true))
+                        .attr("y", d => d.jitter - size_scale(true))
+
+                        // Add number (doesn't work, ignore for now)
+                        self.append("p")
+                            .text("test")
+                            .attr("x", gap_scale(d.gap))
+                            .attr("y", d.jitter)
+                    })
         })
 
-        // Pitstop Rect
-        svg.select("#pitstop-rect")
-            .select("rect")
-                .attr("x", gap_scale(-pitstop_length))
-                .attr("y", 0)
-                .attr("width", gap_scale(0) - gap_scale(-pitstop_length))
-                .attr("height", jitter_height + 10)
-                .style("fill", "yellow")
-                .style("fill-opacity", 0.25)
+        
 
 
     })
